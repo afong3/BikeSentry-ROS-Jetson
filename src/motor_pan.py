971c2847792fc1ac807366d1c324087b153e7cb4
Erik Dahl <erikd234@gmail.com>
2022-01-15 03:46:49 -0500

    Motor Pan code

---
+++ src/motor_pan.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python2.7
+
+import rospy
+from std_msgs.msg import Float32
+import imp
+
+STEPS = 0
+
+CDM = imp.load_source('module.name', '../scripts/CameraDataManager.py')
+MM = imp.load_source('module.name', '../scripts/MotorManager.py')
+
+CameraDataManager = CDM.CameraDataManager(1280, 720)
+Motor = MM.MotorManager(STEPS)
+
+def listener_x():
+    rospy.Subscriber("x_center", Float32, pan_callback)
+
+def pan_callback(pixels_x):
+
+    x = int(pixels_x.data)
+    
+    instr = CameraDataManager.create_motor_instructions_pan(x) 
+
+    Motor.actuate(instr)
+
+
+def main():
+    rospy.init_node("motor_pan")
+    listener_x()
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
