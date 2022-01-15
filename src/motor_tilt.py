11a2923c39ce6e87c931ff8e1cf87ecc2f76512b
Erik Dahl <erikd234@gmail.com>
2022-01-15 14:39:21 -0500

    changing absolute path to relative

--- src/motor_tilt.py
+++ src/motor_tilt.py
@@ -7,11 +7,19 @@
 import rospy
 from std_msgs.msg import Float32
 import imp
+import os
+
+curr_dir = os.path.abspath(os.getcwd()) #'/home/sentry/catkin_ws/src/BikeSentryROS/src/../
+rospy.loginfo(curr_dir)
+repositry_root = curr_dir.split("/src",1)[0]
+rospy.loginfo(repositry_root)
+lib_path = repositry_root + "/scripts"
+rospy.loginfo(lib_path)
+
+CDM = imp.load_source('module.name', lib_path + '/CameraDataManager.py')
+MM = imp.load_source('module.name', lib_path + '/MotorManager.py')
 
 STEPS = 0
-
-CDM = imp.load_source('module.name', '/home/sentry/catkin_ws/src/BikeSentryROS/scripts/CameraDataManager.py') # TODO: make this relative
-MM = imp.load_source('module.name', '/home/sentry/catkin_ws/src/BikeSentryROS/scripts/MotorManager.py')
 
 CameraDataManager = CDM.CameraDataManager(1280, 720)
 Motor = MM.MotorManager(STEPS)
