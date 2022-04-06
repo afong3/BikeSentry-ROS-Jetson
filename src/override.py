92f391bbf7be18423285ee9a69061f6794ad8b23
sentry <erikd234@gmail.com>
2022-04-06 00:10:03 -0700

    Staring the Hackaaatthhonn its late at night

--- src/override.py
+++ src/override.py
@@ -6,25 +6,27 @@ It will put the sentry in sentry mode!
 import rospy
 from std_msgs.msg import String
 
+GLOB_PUB = rospy.Publisher("override", String, queue_size=10)
+
+def clean_up():
+     GLOB_PUB.publish("Running clean up lets go, sweep sweep the broom!")
+     for i in range(1,10):
+        GLOB_PUB.publish("off")
 
 def publisher(pub):
     rospy.init_node("override", anonymous=True)
     rate = rospy.Rate(1)
+    rospy.on_shutdown(clean_up)
     while not rospy.is_shutdown():
         override_message = "on"
         rospy.loginfo("override activated")
         pub.publish(override_message)
         rate.sleep()
 
-
+        
 if __name__ == "__main__":
-    pub = rospy.Publisher("override", String, queue_size=10)
+    pub = GLOB_PUB
     try:
         publisher(pub)
     except rospy.ROSInterruptException:
-        pass
-    finally:
-        pub.publish("Running clean up lets go, sweep sweep the broom!")
-        for i in range(1,10):
-            pub.publish("off")
-        
+       pass 
